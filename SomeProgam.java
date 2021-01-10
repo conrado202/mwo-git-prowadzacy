fca432e0a16fb841299a87353f8ebcea86e8a381
Konrad Bukowicz <konradbukowicz@MacBook-Pro-Konrad.local>
2021-01-10 11:32:48 +0100

    Added sattelite

--- SomeProgam.java
+++ SomeProgam.java
@@ -1,4 +1,4 @@
-public class SomeProgram { 
+public class SomeProgam { 
     public static void main(String[] args) {
       System.out.println("We are learning to use Git.");
       System.out.println("We will be Git experts, soon.");	  
